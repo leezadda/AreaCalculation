2bf032ec7a9ee2d7a16263cabb989351a651fa36
Leo Quezada <74736115+leezadda@users.noreply.github.com>
2021-02-02 19:53:15 -0600

    Add files via upload

---
+++ PA1.py
@@ -0,0 +1,35 @@
+
+"""  Leo Quezada
+     Program #1: The area calculator
+     COSC 1306
+     Spring 2021
+     Calculate a figure's area based on the user's input.
+"""
+Print(Select a figure from the menu:
+1 - Square
+2 - Rectangle
+3 - Triangle)
+
+figureType = input("Enter your selection:")
+sideLength = input("Enter the side length:")
+
+if rectangle {
+Enter the length of the rectangle: 10
+Enter the width of the rectangle: 20
+}
+
+if trianle{
+Enter the base of the triangle: 3
+Enter the height of the triangle: 7
+}
+
+print("The area of the selected figure is: " + x)
+
+
+
+
+
+
+
+
+
