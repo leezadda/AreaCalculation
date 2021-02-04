5751c0bd1766354e0802981b9b08d10a8b2d233b
Leo Quezada <74736115+leezadda@users.noreply.github.com>
2021-02-04 14:28:21 -0600

    updated

--- PA1.py
+++ PA1.py
@@ -6,41 +6,32 @@
      Calculate a figure's area based on the user's input.
 """
 #shows user the menu
-print("Select a figure from the menu:")
-print("1 - Square")
-print("2 - Rectangle")
-print("3 - Triangle")
+print("Select a figure from the menu:\n1 - Square\n2 - Rectangle\n3 - Triangle")
+
 
 #ask user for their shape choice
-figureType = int(input("Enter your selection:"))
+figureType = int(input("Enter your selection: "))
 
 if figureType == 1: #asking for square side length
-    side = int(input("Enter the side length:"))
+    side = int(input("Enter the side length: "))
     square = side*side
     print("The area of the selected figure is: " + "%.2f" %square)
 
 elif figureType == 2: #asking for rectangle length/width
-    length = int(input("Enter the length of the rectangle:"))
-    width = int(input("Enter the width of the rectangle:"))
+    length = int(input("Enter the length of the rectangle: "))
+    width = int(input("Enter the width of the rectangle: "))
     rectangle = length * width
     print("The area of the selected figure is: " + "%.2f" %rectangle)
 
 
 elif figureType == 3: #asking for triangle base/height
-    base = int(input("Enter the base of the triangle:"))
-    height = int(input("Enter the height of the triangle:"))
+    base = int(input("Enter the base of the triangle: "))
+    height = int(input("Enter the height of the triangle: "))
     triangle = (base * height)/2
     print("The area of the selected figure is: " + "%.2f" %triangle)
 
 else:
     print("Invalid option")
-
-#final output
-#print("The area of the selected figure is: " + "%.2f" %variable_name)
-
-
-
-
 
 
 
